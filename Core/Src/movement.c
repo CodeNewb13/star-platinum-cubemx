c24bbf47b59119c8c4741df3be2783dcf6de9527
oihv <kokolas16@gmail.com>
2025-05-03 21:40:52 +0800

    Implement some movements with mahony filter instead of greyscale
    This includes, move forward, left, and right, backwards movement left

--- Core/Src/movement.c
+++ Core/Src/movement.c
@@ -1,5 +1,6 @@
 #include "movement.h"
 #include "motor.h"
+#include "pid.h"
 #include "stm32f1xx_hal.h"
 
 /*
@@ -9,30 +10,32 @@
  * front left <= 1 4 => front right
  *  rear left <= 2 3 => rear right
  */
-
+extern PID_Controller pid;
+extern float yaw;
 int calculate_Motor_RPM(const int baseSpeed, int pid) {
   if (pid < 0)
     return 0;
   else
     return pid;
 }
+
 int pidcorrection;
 void moveForward(int rpm) {
-  while (1) {
-    // if(FourLineCross()){
-    //   stopMotor();
-    //   return;
-    // }
-    // else{
-    pidcorrection = PID();
-    // pidcorrection = calculate_Motor_RPM(rpm, pidcorrection);
-    Set_Motor1_RPM(rpm - pidcorrection);
-    Set_Motor2_RPM(rpm - pidcorrection);
-    Set_Motor3_RPM(rpm + pidcorrection);
-    Set_Motor4_RPM(rpm + pidcorrection);
-    HAL_Delay(10);
-    //}
-  }
+  // while (1) {
+  // if(FourLineCross()){
+  //   stopMotor();
+  //   return;
+  // }
+  // else{
+  pidcorrection = PID_Compute(&pid, 0.0, yaw, 10);
+  // pidcorrection = calculate_Motor_RPM(rpm, pidcorrection);
+  Set_Motor1_RPM(rpm - pidcorrection);
+  Set_Motor2_RPM(rpm - pidcorrection);
+  Set_Motor3_RPM(rpm + pidcorrection);
+  Set_Motor4_RPM(rpm + pidcorrection);
+  // HAL_Delay(10);
+  //}
+  // }
 }
 
 void moveBackward(int rpm) {
@@ -48,47 +51,57 @@ void moveBackward(int rpm) {
 
 void moveLeft(int rpm) {
 
-  motorLeft(rpm);
-  bool leftInitialLine = false;
-
-  while (!leftInitialLine) {
-    readGreyscale();
-    bool lineDetected = false;
-    for (int i = 0; i < 7; i++) {
-      if (sensor[i] == 0) {
-        lineDetected = true;
-        break;
-      }
-    }
-    if (!lineDetected) {
-      leftInitialLine = true; // We've left the starting line
-    }
-  }
-  HAL_Delay(50);
-
-  while (1) {
-    int status = LinePositionStatus();
-    if (status == 1)
-      motorLeft(rpm / 4); // lower speed
-    else if (status == 2) {
-      stopMotor();
-      return;
-    }
-  }
+  // motorLeft(rpm);
+  // bool leftInitialLine = false;
+  //
+  // while (!leftInitialLine) {
+  //   readGreyscale();
+  //   bool lineDetected = false;
+  //   for (int i = 0; i < 7; i++) {
+  //     if (sensor[i] == 0) {
+  //       lineDetected = true;
+  //       break;
+  //     }
+  //   }
+  //   if (!lineDetected) {
+  //     leftInitialLine = true; // We've left the starting line
+  //   }
+  // }
+  // HAL_Delay(50);
+  //
+  // while (1) {
+  //   int status = LinePositionStatus();
+  //   if (status == 1)
+  //     motorLeft(rpm / 4); // lower speed
+  //   else if (status == 2) {
+  //     stopMotor();
+  //     return;
+  //   }
+  // }
+  pidcorrection = PID_Compute(&pid, 0.0, yaw, 10);
+  Set_Motor1_RPM(-(rpm + pidcorrection));
+  Set_Motor2_RPM(rpm - pidcorrection);
+  Set_Motor3_RPM(-(rpm - pidcorrection));
+  Set_Motor4_RPM(rpm + pidcorrection);
 }
 
 void moveRight(int rpm) {
-  while (1) {
-    int status = LinePositionStatus();
-    if (!status)
-      motorRight(rpm);
-    else if (status == 1)
-      motorRight(rpm / 4); // lower speed
-    else if (status == 2) {
-      stopMotor();
-      return;
-    }
-  }
+  // while (1) {
+  //   int status = LinePositionStatus();
+  //   if (!status)
+  //     motorRight(rpm);
+  //   else if (status == 1)
+  //     motorRight(rpm / 4); // lower speed
+  //   else if (status == 2) {
+  //     stopMotor();
+  //     return;
+  //   }
+  // }
+  pidcorrection = PID_Compute(&pid, 0.0, yaw, 10);
+  Set_Motor1_RPM(rpm - pidcorrection);
+  Set_Motor2_RPM(-(rpm + pidcorrection));
+  Set_Motor3_RPM(rpm + pidcorrection);
+  Set_Motor4_RPM(-(rpm - pidcorrection));
 }
 
 void rotateCW(int rpm) {
