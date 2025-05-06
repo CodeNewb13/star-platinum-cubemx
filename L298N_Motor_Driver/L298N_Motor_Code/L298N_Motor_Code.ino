5212f8959307f49003032a7423b44e6915d47dc2
Justin <justin.surlaya@gmail.com>
2025-05-07 02:46:55 +0800

    Removed the code for L298N Motor Driver, used a more simple implementation in the new L298N_Motor_Code.ino file

---
+++ L298N_Motor_Driver/L298N_Motor_Code/L298N_Motor_Code.ino
@@ -0,0 +1,41 @@
+const int enA = 9, in1 = 8, in2 = 7;
+
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  // power up logic outputs
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0);
+  delay(1000);
+
+  // 1) Drive enable only
+  analogWrite(enA, 255);
+
+  // 2) Forward direction
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  delay(3000);
+
+/*
+  // 3) Reverse direction
+  Serial.println("Now IN1=LOW, IN2=HIGH: measure OUT1/2");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(3000);
+  */
+
+  /*
+
+  // 4) Off
+  Serial.println("Turning OFF");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0);
+*/
+
+}
+
+void loop() { }
